4f92e1d6d069504db6a36c31e7cd33105058d6de
User <User@host.docker.internal>
2025-10-23 23:30:05 +0530

    spring boot profile commit

--- EmployeeManagementProject/src/main/java/com/nt/serviceImpl/EmployeeServiceImpl.java
+++ EmployeeManagementProject/src/main/java/com/nt/serviceImpl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.nt.serviceImpl;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import com.nt.entity.Employee;
@@ -10,6 +11,7 @@ import com.nt.repository.EmployeeRepo;
 import com.nt.service.EmployeeService;
 
 @Service
+@Profile({"dev", "qa"})
 public class EmployeeServiceImpl implements EmployeeService {
 	
 	@Autowired
